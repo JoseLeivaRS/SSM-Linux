071f4e15c9f9ed806b42e8d83c0f4e2bcb32c688
JoseLeivaRS <jose.leiva@rackspace.com>
2022-09-23 23:54:11 -0500

    systemctl status added --no-pager argument
    Fixed a few settings on commands

--- linuxremediationscript.sh
+++ linuxremediationscript.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 detect_distro(){
   DISTRO=$( ( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1| grep -Poi '(debian|ubuntu|red hat|centos|suse|oracle)' | uniq )
-if [ -z $DISTRO ]; then
+  if [ -z $DISTRO ]; then
     DISTRO='unknown'
-fi
-echo $DISTRO  #work on fixing this 
+  fi
+  echo $DISTRO  #Added this to review output for testing
 }
-detect_distro 
+
+RunningDistro=$(detect_distro)
+
 install_ssm(){
   sudo curl -sL https://add-ons.manage.rackspace.com/bootstrap/azure/ssm_install.sh --output /tmp/ssm_install.sh  
   #INSTALLOUTPUT=$( chmod +x /tmp/ssm_install.sh; sudo sh -s /tmp/ssm_install.sh )
@@ -30,10 +32,25 @@ uninstall_ssm(){
     ;;
 esac
 }
-get_status(){
 
+get_status(){
+  case $RunningDistro in
+  Centos )
+  echo Running $RunningDistro ... Checking status
+    sudo systemctl status --no-pager amazon-ssm-agent
+  ;;
+  Ubuntu )
+  echo Running $RunningDistro ... Checking status
+    sudo systemctl status --no-pager amazon-ssm-agent
+  ;;
+  * )
+  not recognized
+  ;;
+esac
 }
+
 REGION="us-west-2"
+test_endpoints() {
 SSMEndPT=("ssm.$REGION.amazonaws.com" "ssmmessages.$REGION.amazonaws.com" "ec2messages.$REGION.amazonaws.com" "s3.$REGION.amazonaws.com" "add-ons.api.manage.rackspace.com" "add-ons.manage.rackspace.com")
 #TEST=@()
 #TESTOUTPUT=""
@@ -45,10 +62,15 @@ for URL in ${SSMEndPT[@]}
 
     #echo $URL
 done
+}
 
-insta
+get_status
+test-test_endpoints
+install_ssm
+get_status
+uninstall_ssm
+echo ""
 
-sudo systemctl status amazon-ssm-agent
 
 amazon-ssm-agent -register -clear
 
